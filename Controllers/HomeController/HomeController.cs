54e042c80c49ac5e7acea9f1cfa0041678da931c
dmkrebsbach <damiank@uw.edu>
2019-08-06 14:54:04 -0700

    changes Portfolio 1 to Portfolio 2

--- Controllers/HomeController/HomeController.cs
+++ Controllers/HomeController/HomeController.cs
@@ -1,30 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 namespace helloWorld
 {
-    public class homeController : Controller 
+    public class HomeController : Controller // Home is the name of the directory where all of these actions/views should reference
     {
-        //Requests
-        //localhost:5000/
-        [HttpGet("")]
-        public string Index()
+        [HttpGet]
+        [Route("")]
+        public ViewResult Index()
         {
-            return "This is my Index!!!";
-        }
-
-        //localhost:5000/hello
-        [HttpGet("projects")]
-        public string Projects()
-        {
-            return "These are my projects";
-        }
-
-        //localhost:5000/contacts
-        [HttpGet("contacts")]
-        public string Contacts()
-        {
-            return "This is My Contact Info!";
+            return View();
         }
 
 
+        [HttpGet]
+        [Route("projects")]
+        public ViewResult Projects()
+        {
+            return View("Projects");
+        }
+        
+        [HttpGet]
+        [Route("contact")]
+        public ViewResult Contact()
+        {
+            return View("Contact");
+        }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace Portfolio1
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
